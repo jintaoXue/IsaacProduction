0503f759e502c91bdcba393a6d5c9c418f58abf3
xue <thecaichicken@gmail.com>
2024-12-02 00:51:56 +0800

    add transformer

--- omniisaacgymenvs/algo/rainbowmini/model.py
+++ omniisaacgymenvs/algo/rainbowmini/model.py
@@ -485,3 +485,352 @@ class MultiheadAttentionGlobalHead(nn.Module):
         type_embedding = type_embedding.transpose(0, 1)
         outputs, attns = self.encoder(inputs[[0]], inputs + type_embedding, inputs)
         return outputs.squeeze(0), attns.squeeze(1)
+    
+
+
+class FeatureEmbeddingBlock(nn.Module):
+  def __init__(self, hidden_size, dimstate : DimState):
+    super().__init__()
+    self.dim_state = dimstate
+    self.dtype = torch.float32
+    self.hidden_size = hidden_size
+    self.action_mask_embedding= nn.Sequential(
+        nn.Linear(dimstate.action_mask, hidden_size), nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+    )
+    self.time_step_embedding = nn.Sequential(
+        nn.Linear(dimstate.time_step, hidden_size), nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+    )
+    self.progress_emb = nn.Sequential(
+        nn.Linear(dimstate.progress, hidden_size), nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+    )
+    self.state_depot_hoop_embedding = nn.Embedding(dimstate.types_state_depot_hoop, hidden_size)
+    self.have_raw_hoops_embedding = nn.Embedding(dimstate.types_have_raw_hoops, hidden_size)
+    self.state_depot_bending_tube_embedding= nn.Embedding(dimstate.types_state_depot_bending_tube, hidden_size)
+    self.have_raw_bending_tube_embedding = nn.Embedding(dimstate.types_have_raw_bending_tube, hidden_size)
+    self.station_state_inner_left_embedding = nn.Embedding(dimstate.types_station_state_inner_left, hidden_size)
+    self.station_state_inner_right_embedding = nn.Embedding(dimstate.types_station_state_inner_right, hidden_size)
+    self.station_state_outer_left_embedding = nn.Embedding(dimstate.types_station_state_outer_left, hidden_size)
+    self.station_state_outer_right_embedding = nn.Embedding(dimstate.types_station_state_outer_right, hidden_size)
+    self.cutting_machine_state_embedding = nn.Embedding(dimstate.types_cutting_machine_state, hidden_size)
+    self.is_full_products_embedding = nn.Embedding(dimstate.types_is_full_products, hidden_size)
+    self.produce_product_req_embedding = nn.Embedding(dimstate.types_produce_product_req, hidden_size)
+
+    self.worker_state_embd = nn.Embedding(dimstate.types_worker_state, hidden_size)
+    self.worker_task_embd = nn.Embedding(dimstate.types_worker_task, hidden_size)
+    self.worker_pose_embd = nn.Embedding(dimstate.types_worker_pose, hidden_size)
+    self.agv_state_embd = nn.Embedding(dimstate.types_agv_state, hidden_size)
+    self.agv_task_embd = nn.Embedding(dimstate.types_agv_task, hidden_size)
+    self.agv_pose_embd = nn.Embedding(dimstate.types_agv_pose, hidden_size)
+    self.box_state_embd = nn.Embedding(dimstate.types_box_state, hidden_size)
+    self.box_task_embd = nn.Embedding(dimstate.types_box_task, hidden_size)
+
+  def forward(self, state, **kwargs):
+
+    action_mask_embedding = self.action_mask_embedding(state['action_mask'])
+    time_step_embedding = self.time_step_embedding(state['time_step'])
+    progress = self.progress_emb(state['progress'])
+
+    #operation: * math.sqrt(self.hidden_size) according to the paper attention is all you need
+    state_depot_hoop_embedding= self.state_depot_hoop_embedding(state['state_depot_hoop'])
+    have_raw_hoops_embedding= self.have_raw_hoops_embedding(state['have_raw_hoops'])
+    state_depot_bending_tube_embedding= self.state_depot_bending_tube_embedding(state['state_depot_bending_tube'])
+    have_raw_bending_tube_embedding = self.have_raw_bending_tube_embedding(state['have_raw_bending_tube'])
+    station_state_inner_left_embedding = self.station_state_inner_left_embedding(state['station_state_inner_left'])
+    station_state_inner_right_embedding = self.station_state_inner_right_embedding(state['station_state_inner_right'])
+    station_state_outer_left_embedding = self.station_state_outer_left_embedding(state['station_state_outer_left'])
+    station_state_outer_right_embedding = self.station_state_outer_right_embedding(state['station_state_outer_right'])
+    cutting_machine_state_embedding = self.cutting_machine_state_embedding(state['cutting_machine_state'])
+    is_full_products_embedding = self.is_full_products_embedding(state['is_full_products'])
+    produce_product_req_embedding = self.produce_product_req_embedding(state['produce_product_req'])
+    worker_state_0 = self.worker_state_embd(state['worker_state_0'])
+    worker_state_1 = self.worker_state_embd(state['worker_state_1'])
+    worker_task_0 = self.worker_task_embd(state['worker_task_0'])
+    worker_task_1 = self.worker_task_embd(state['worker_task_1'])
+    worker_pose_0 = self.worker_pose_embd(state['worker_pose_0'])
+    worker_pose_1 = self.worker_pose_embd(state['worker_pose_1'])
+    agv_state_0 = self.agv_state_embd(state['agv_state_0'])
+    agv_state_1 = self.agv_state_embd(state['agv_state_1'])
+    agv_task_0 = self.agv_task_embd(state['agv_task_0'])
+    agv_task_1 = self.agv_task_embd(state['agv_task_1'])
+    agv_pose_0 = self.agv_pose_embd(state['agv_pose_0'])
+    agv_pose_1 = self.agv_pose_embd(state['agv_pose_1'])
+    box_state_0 = self.box_state_embd(state['box_state_0'])
+    box_state_1 = self.box_state_embd(state['box_state_1'])
+    box_task_0 = self.box_task_embd(state['box_task_0'])
+    box_task_1 = self.box_task_embd(state['box_task_1'])
+    box_pose_0 = self.agv_pose_embd(state['box_pose_0'])
+    box_pose_1 = self.agv_pose_embd(state['box_pose_1'])
+
+    ###########################################################################################
+    ###########################################################################################
+
+    all_embs = torch.cat([action_mask_embedding.unsqueeze(1), state_depot_hoop_embedding, have_raw_hoops_embedding, state_depot_bending_tube_embedding, 
+                          have_raw_bending_tube_embedding, station_state_inner_left_embedding, station_state_inner_right_embedding, 
+                          station_state_outer_left_embedding, station_state_outer_right_embedding, cutting_machine_state_embedding, 
+                          is_full_products_embedding, produce_product_req_embedding, time_step_embedding.unsqueeze(1), progress.unsqueeze(1), 
+                          worker_state_0, worker_task_0, worker_pose_0, worker_state_1, worker_task_1, worker_pose_1, 
+                          agv_state_0, agv_task_0, agv_pose_0, agv_state_1, agv_task_1, agv_pose_1,
+                          box_state_0, box_task_0, box_pose_0, box_state_1, box_task_1, box_pose_1], dim=1)
+
+    return all_embs* math.sqrt(self.hidden_size)
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
+        # Apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        return self.dropout(x)
+
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
+
+class MultiheadAttentionBlock(nn.Module):
+    def __init__(self, d_model, nhead=8, dropout=0.1):
+        super().__init__()
+        self.encoder = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=True)
+
+    def forward(self, q, k, v, mask):
+        outputs, attns = self.encoder(q, k, v, mask)
+        return outputs
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, features: int, self_attention_block: MultiheadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, features: int, cross_attention_block: MultiheadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        # self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        # x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+    
+class Decoder(nn.Module):
+
+    def __init__(self, features: int, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model, vocab_size) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x) -> None:
+        # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+        return self.proj(x)
+    
+class Transformer(nn.Module):
+
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: FeatureEmbeddingBlock, tgt_embed: InputEmbeddings, 
+          src_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        # (batch, seq_len, d_model)
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output: torch.Tensor, src_mask: torch.Tensor, tgt: torch.Tensor, tgt_mask: torch.Tensor):
+        # (batch, seq_len, d_model)
+        tgt = self.tgt_embed(tgt).unsqueeze(1)
+        # tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def project(self, x):
+        # (batch, seq_len, vocab_size)
+        return self.projection_layer(x)
+    
+    def forward(self, state):
+        src = self.encode(state, None)
+        tgt = self.decode(src, None, state['action_mask'], None)
+        return self.projection_layer(tgt)
+    
+
+def build_transformer(dim_state: DimState, d_model: int=512, h: int=8, dropout: float=0.1, d_ff: int=2048) -> Transformer:
+    # Create the embedding layers
+    src_embed = FeatureEmbeddingBlock(d_model, dim_state)
+    tgt_embed = nn.Sequential(
+            nn.Linear(dim_state.action_mask, d_model), nn.ReLU(),
+            nn.Linear(d_model, d_model),
+        )
+    # Create the positional encoding layers
+    src_pos = PositionalEncoding(d_model, dim_state.src_seq_len, dropout)
+    
+    # Create the encoder blocks
+    encoder_blocks = []
+    for _ in range(2):
+        encoder_self_attention_block = MultiheadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block = EncoderBlock(d_model, encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
+
+    # Create the decoder blocks
+    decoder_blocks = []
+    for _ in range(1):
+        decoder_cross_attention_block = MultiheadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block = DecoderBlock(d_model, decoder_cross_attention_block, feed_forward_block, dropout)
+        decoder_blocks.append(decoder_block)
+    
+    # Create the encoder and decoder
+    encoder = Encoder(d_model, nn.ModuleList(encoder_blocks))
+    # decoder = None
+    decoder = Decoder(d_model, nn.ModuleList(decoder_blocks))
+    
+    # Create the projection layer
+    projection_layer = FeatureMapper(d_model, d_model)
+    # Create the transformer
+    transformer = Transformer(encoder, decoder, src_embed, tgt_embed, src_pos, projection_layer)
+    
+    # Initialize the parameters
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    
+    return transformer
+
+class DQNTrans(nn.Module):
+  def __init__(self, config, action_space):
+    super(DQNTrans, self).__init__()
+    self.action_space = action_space
+    hidden_size = config['hidden_size']
+    self.transformer : Transformer = build_transformer(DimState(), hidden_size) 
+    self.fc_h_v = NoisyLinear(hidden_size, hidden_size, std_init=config['noisy_std'])
+    self.fc_h_a = NoisyLinear(hidden_size, hidden_size, std_init=config['noisy_std'])
+    self.fc_z_v = NoisyLinear(hidden_size, 1, std_init=config['noisy_std'])
+    self.fc_z_a = NoisyLinear(hidden_size, action_space, std_init=config['noisy_std'])
+    self.Vmin = config.get('V_min', -20)
+    self.Vmax = config.get('V_max', 20)
+  def forward(self, x, log=False):
+    action_mask = x['action_mask']
+    x = self.transformer(x)
+    x = x.squeeze(1) # squeeze the query sequence
+    v = self.fc_z_v(F.relu(self.fc_h_v(x)))  # Value stream
+    a = self.fc_z_a(F.relu(self.fc_h_a(x)))  # Advantage stream
+    q = v + a - a.mean(1, keepdim=True)  # Combine streams
+    # q = torch.nn.functional.normalize(q, dim=1)
+    assert log==False, "rainbowmini only support log False"
+    # if log:  # Use log softmax for numerical stability
+    #   q = F.log_softmax(q, dim=1)  # Log probabilities with action over second dimension
+    # else:
+    q = torch.clamp(q, min=self.Vmin, max=self.Vmax)
+    q = (action_mask-1)*(-self.Vmin) + q*action_mask
+    return q
+
+  def reset_noise(self):
+    for name, module in self.named_children():
+      if 'fc' in name:
+        module.reset_noise()
