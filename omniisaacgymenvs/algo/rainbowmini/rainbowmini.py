891f23f611aecb350e981f1287f4fc3f65e9509e
xue <thecaichicken@gmail.com>
2024-12-06 01:13:45 +0800

    v5 comprehensive evaluation

--- omniisaacgymenvs/algo/rainbowmini/rainbowmini.py
+++ omniisaacgymenvs/algo/rainbowmini/rainbowmini.py
@@ -42,7 +42,7 @@ class RainbowminiAgent():
         self.max_epochs = config.get("max_epochs", int(1e11))
         self.batch_size = config.get('batch_size', 512)
         # self.batch_size = config.get('batch_size', 2)
-        self.num_warmup_steps = config.get('num_warmup_steps', int(5e4))
+        self.num_warmup_steps = config.get('num_warmup_steps', int(10e4))
         # self.num_warmup_steps = config.get('num_warmup_steps', int(1024))
         self.demonstration_steps = config.get('demonstration_steps', int(0))
         self.num_steps_per_epoch = config.get("num_steps_per_epoch", 100)
