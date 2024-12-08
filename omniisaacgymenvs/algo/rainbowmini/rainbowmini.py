0d9ab4026eb8b73eb47c27cff44f92650f381ad4
xue <thecaichicken@gmail.com>
2024-12-08 15:42:23 +0800

    v5 target_updtae freq

--- omniisaacgymenvs/algo/rainbowmini/rainbowmini.py
+++ omniisaacgymenvs/algo/rainbowmini/rainbowmini.py
@@ -36,7 +36,7 @@ class RainbowminiAgent():
         ###########for agent training
         self.update_frequency = config.get('update_frequency', 200)
         self.evaluate_interval = config.get('evaluate_interval', 100)
-        self.target_update = config.get('target_update', int(1e2))
+        self.target_update = config.get('target_update', int(2e3))
         self.max_steps = config.get("max_steps", int(7e5))
         self.max_epochs = config.get("max_epochs", int(1e11))
         self.batch_size = config.get('batch_size', 512)
