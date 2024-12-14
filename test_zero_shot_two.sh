c40006c350c5e450087decfb45c22e008c8be586
xue <thecaichicken@gmail.com>
2024-12-15 02:38:53 +0800

    drawing

--- test_zero_shot_two.sh
+++ test_zero_shot_two.sh
@@ -1,23 +1,23 @@
 #！/bin/bash
 # 读取不同的学习率
-load_dir="/FactoryTaskAllocationMiC_2024-12-11_13-29-49/nn"
-load_dir2="/FactoryTaskAllocationMiC_2024-12-09_14-31-02/nn"
-load_dir3="/FactoryTaskAllocationMiC_2024-12-08_15-44-10/nn"
+load_dir="/FactoryTaskAllocationMiC_2024-12-08_15-44-10/nn"
+load_dir2="/FactoryTaskAllocationMiC_2024-12-10_13-06-23/nn"
+# load_dir3="/FactoryTaskAllocationMiC_2024-12-08_15-44-10/nn"
 # load_name="/FactoryTaskAllocationMiC_ep_25000.pth"
-train=edqn
-train2=FactoryTaskAllocationMiCepsilon_noisy
-train3=FactoryTaskAllocationMiCRainbowmini
+train=FactoryTaskAllocationMiCRainbowmini
+train2=no_dueling
+# train3=FactoryTaskAllocationMiCRainbowmini
 
 relative_pth="/omniisaacgymenvs/runs"
 str="/"
 work_space_path=$(pwd)
 dir_path=$work_space_path$relative_pth$load_dir
 dir_path2=$work_space_path$relative_pth$load_dir2
-dir_path3=$work_space_path$relative_pth$load_dir3
+# dir_path3=$work_space_path$relative_pth$load_dir3
 # path=$1
 files=$(ls $dir_path)
 files2=$(ls $dir_path2)
-files3=$(ls $dir_path3)
+# files3=$(ls $dir_path3)
 list=(
     1
     2
@@ -57,16 +57,18 @@ done
 done
 
 
-for filename3 in $files3
-do
-for num in $list
-do
-    python omniisaacgymenvs/scripts/rlgames_train_v1.py task=FactoryTaskAllocationMiC train=$train3 headless=True wandb_activate=True test=True \
-    load_dir="$load_dir3" load_name="$str$filename3" wandb_project=test_zero_shot test_times=10 num_product=$num
-#    echo $filename >> filename.txt
-#    echo -e >> filename.txt
-done
-done
+# for filename3 in $files3
+# do
+# for num in $list
+# do
+#     python omniisaacgymenvs/scripts/rlgames_train_v1.py task=FactoryTaskAllocationMiC train=$train3 headless=True wandb_activate=True test=True \
+#     load_dir="$load_dir3" load_name="$str$filename3" wandb_project=test_zero_shot test_times=10 num_product=$num
+# #    echo $filename >> filename.txt
+# #    echo -e >> filename.txt
+# done
+# done
+
+
 # for num in $list
 # do
 #     python omniisaacgymenvs/scripts/rlgames_train_v1.py task=FactoryTaskAllocationMiC train=FactoryTaskAllocationMiCRainbowepsilon headless=True wandb_activate=True test=True \
