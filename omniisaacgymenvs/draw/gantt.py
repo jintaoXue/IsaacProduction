1035c4d043161158fc3f9439fe4196bdf9ee6e38
xue <thecaichicken@gmail.com>
2024-12-17 22:36:06 +0800

    makespan

--- omniisaacgymenvs/draw/gantt.py
+++ omniisaacgymenvs/draw/gantt.py
@@ -7,7 +7,7 @@ def get_cmap(n, name='hsv'):
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
     # return plt.colormaps.get_cmap(name, n)
-cmap = get_cmap(12)
+cmap = get_cmap(11)
 color_dict_inv = {0: 'none', 1: 'hoop_preparing', 2:'bending_tube_preparing', 3:'hoop_loading_inner', 4:'bending_tube_loading_inner', 5:'hoop_loading_outer', 
     6:'bending_tube_loading_outer', 7:'cutting_cube', 8:'collect_product', 9:'placing_product'}
 color_dict =  {v: k for k, v in color_dict_inv.items()}
@@ -16,29 +16,46 @@ y_pos_dic = {'robot1':(10,5),'robot0':(20,5), 'human1':(30,5), 'human0':(40,5), 
 
 y_ticklabels = list(y_pos_dic.keys())
 y_ticks = [15-2.5, 25-2.5, 35-2.5,45-2.5, 55-2.5]
-def draw(ax, data_list, title):
-
+def draw(ax, data_list, title, vis_flag):
+    labeled_color_dict = {}
     for data in data_list:
         for y_label, color, s_time, diff_time in data:
-            ax.broken_barh([(s_time, diff_time)], y_pos_dic[y_label], facecolors=cmap(color),edgecolor="black",zorder=2)
+            if y_label == 'action':
+                ax.arrow(s_time, sum(y_pos_dic[y_label]), 0, -2, color=cmap(color), width=1.0, shape='full', head_starts_at_zero=False)
+                continue
+            if color not in labeled_color_dict.keys():
+                labeled_color_dict[color] = 'labeled'
+                ax.broken_barh([(s_time, diff_time)], y_pos_dic[y_label], facecolors=cmap(color),edgecolor="black", label="task_{}".format(color),zorder=2)
+            else:
+                ax.broken_barh([(s_time, diff_time)], y_pos_dic[y_label], facecolors=cmap(color),edgecolor="black",zorder=2)
+
 
     ax.set_ylim(5, 60)
     ax.set_xlim(-5, 900)
     for spine in ["top","left","right"]:
         ax.spines[spine].set_color('none')
-
+    if vis_flag:
+        ax.set_xlabel('Timespan',fontsize=15)
+        # ax.legend()
+        # reordering the labels 
+        handles, labels = ax.get_legend_handles_labels() 
+        idx = [ int(label.split('_')[-1])  for label in labels]
+        order = np.argsort(idx) 
+        # pass handle & labels lists along with order as below 
+        ax.legend([handles[i] for i in order], [labels[i] for i in order], frameon=True,ncol=10,loc = "lower center",bbox_to_anchor=(0.5, -.25)) 
     ax.spines["bottom"].set_linewidth(1)
     ax.tick_params(direction="out",left=False)
     ax.tick_params()
-    ax.set_xlabel('Seconds Since Start',fontsize=15)
-    ax.set_ylabel('Project Name ',fontsize=15)
+        
+    # ax.set_ylabel('Project Name ',fontsize=15)
     ax.set_yticks(y_ticks)
-    ax.set_yticklabels(y_ticklabels)
+    ax.set_yticklabels(y_ticklabels, fontsize=15)
     ax.grid(linestyle="-",linewidth=.5,color="gray",alpha=.6)
-    ax.set_title(title,pad=10,fontsize=16,fontweight="bold")
-    ax.legend(frameon=False,ncol=4,loc = "lower center",bbox_to_anchor=(0.5, -.3))
-    ax.text(.85,.05,'Visualization by DataCharm',transform = ax.transAxes,
-                ha='center', va='center',fontsize = 9,color='black')
+    # ax.set_title(title,pad=10,fontsize=20,fontweight="bold")
+    ax.set_title(title,fontsize=20)
+    
+    # ax.text(.85,.05,'Visualization by DataCharm',transform = ax.transAxes,
+    #             ha='center', va='center',fontsize = 9,color='black')
 
 
 def proprocess(data : dict):
@@ -109,7 +126,7 @@ if __name__ == '__main__':
     noe_date = proprocess(data_noe)
 
 
-    fig = plt.figure(figsize=(20,5), dpi=100)
+    fig = plt.figure(figsize=(20,12), dpi=100)
     ax1 = plt.subplot(211)
     ax2 = plt.subplot(212)
     plt.style.use('seaborn-v0_8-white')
@@ -118,10 +135,10 @@ if __name__ == '__main__':
             'legend.handlelength': 2}
     plt.rcParams.update(params)
 
-    draw(ax1, n_date, 'EQX-N')
-    draw(ax2, noe_date, 'D3QN')
+    draw(ax1, n_date, 'EQX-N', False)
+    draw(ax2, noe_date, 'D3QN', True)
 
     plt.tight_layout()
-    plt.show()
+    # plt.show()
     path = os.path.dirname(__file__)
-    fig.savefig('{}.pdf'.format(path + '/' + 'gant'), bbox_inches='tight')
+    fig.savefig('{}.pdf'.format(path + '/' + 'gantt'), bbox_inches='tight')
