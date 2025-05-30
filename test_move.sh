465968651f0e4ffbd4f813bcfdbceff642e41c0c
xue <thecaichicken@gmail.com>
2025-05-30 13:44:44 +0800

    5.28 short move version

--- test_move.sh
+++ test_move.sh
@@ -37,49 +37,87 @@ files=$(ls $dir_path)
 
 
 
-#####short
+
+
+
+
+############### short new
 
 list=(
-    24600
-    23600
-    23400
-    23300
-    23100
-    22900
-    22600
-    22200
-    21800
-    # 21100
-    21000
-    20800
-    20700
-    20600
-    20500
-    20300
-    20000
-    19800
-    19500
-    19300
-    19000
-    18900
-    18800
-    18700
-    18600
-    18500
-    18200
-    17500
-    17400
-    16900
-    16600
-    16200
-    15500
-    15300
+ 52900
+ 51800
+ 50000
+ 49300
+ 42000
+ 39600
 )
 
 for num in "${list[@]}"
 do
     python omniisaacgymenvs/scripts/rlgames_train_v1.py task=FactoryTaskAllocationMiC train=FactoryTaskAllocationMiCRainbowmini headless=True wandb_activate=True test=True \
-        load_dir="/FactoryTaskAllocationMiC_2025-05-24_13-25-03/nn" load_name=/FactoryTaskAllocationMiC_ep_$num.pth wandb_project=test_move test_times=100
+        load_dir="/FactoryTaskAllocationMiC_2025-05-28_22-16-18/nn" load_name=/FactoryTaskAllocationMiC_ep_$num.pth wandb_project=test_move test_times=100
     # echo $num
 #    echo -e >> filename.txt
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####short
+
+# list=(
+#     24600
+#     23600
+#     23400
+#     23300
+#     23100
+#     22900
+#     22600
+#     22200
+#     21800
+#     # 21100
+#     21000
+#     20800
+#     20700
+#     20600
+#     20500
+#     20300
+#     20000
+#     19800
+#     19500
+#     19300
+#     19000
+#     18900
+#     18800
+#     18700
+#     18600
+#     18500
+#     18200
+#     17500
+#     17400
+#     16900
+#     16600
+#     16200
+#     15500
+#     15300
+# )
+
+# for num in "${list[@]}"
+# do
+#     python omniisaacgymenvs/scripts/rlgames_train_v1.py task=FactoryTaskAllocationMiC train=FactoryTaskAllocationMiCRainbowmini headless=True wandb_activate=True test=True \
+#         load_dir="/FactoryTaskAllocationMiC_2025-05-24_13-25-03/nn" load_name=/FactoryTaskAllocationMiC_ep_$num.pth wandb_project=test_move test_times=100
+#     # echo $num
+# #    echo -e >> filename.txt
+# done
+
